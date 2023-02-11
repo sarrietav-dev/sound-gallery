04b563df0f7bfe4d07fded48d531bbae2e907fad
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-11 11:55:32 -0500

    🔧 Configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AudioPlayerModule } from './components/audio-player/audio-player.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { AudioPlayerModule } from './components/audio-player/audio-player.module
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AudioPlayerModule
+    AudioPlayerModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'sound-gallery-b433a',
+    appId: '1:99589144670:web:9e7d3f844341b7b2bff4cf',
+    storageBucket: 'sound-gallery-b433a.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC9IUInvbF61Y1_lMdluywEW4yIzE_RjUc',
+    authDomain: 'sound-gallery-b433a.firebaseapp.com',
+    messagingSenderId: '99589144670',
+    measurementId: 'G-3MYK89C43L',
+  },};
