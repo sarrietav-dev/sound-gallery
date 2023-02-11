8c012dabe2b7617ce813c1c59a2eac07741840b3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-11 11:09:11 -0500

    🚧 Add props to the component

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'audio-player',
   templateUrl: './audio-player.component.html'
 })
 export class AudioPlayerComponent {
-
+  @Input() title: string = "Audio Player";
+  @Input() audioNumber: number = 0;
 }
--- src/app/components/audio-player/audio-player.module.ts
+++ src/app/components/audio-player/audio-player.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconPlayComponent } from './components/icon-play/icon-play.component';
 import { IconPauseComponent } from './components/icon-pause/icon-pause.component';
-import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import { AudioPlayerComponent } from './audio-player.component';
 
 
 
