4807ef5a9530177a57eac5ba2ce68fc45bdf23a2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-19 19:11:18 -0500

    ✨ Add spinner to the index component

--- src/app/modules/index/index-page.component.ts
+++ src/app/modules/index/index-page.component.ts
@@ -10,12 +10,15 @@ import { Audio } from '@core/models/Audio';
 export class IndexPageComponent {
   title = 'sound-gallery';
   audios: Audio[] = [];
+  isLoading = false
 
   constructor(private audioService: AudioStorageService) {}
 
   ngOnInit(): void {
+    this.isLoading = true
     this.audioService.getAudios().subscribe((audios) => {
       this.audios = audios;
+      this.isLoading = false
     });
   }
 }
--- src/app/modules/index/index.module.ts
+++ src/app/modules/index/index.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexPageComponent } from './index-page.component';
 import { AudioPlayerModule } from '@app/shared/components/audio-player/audio-player.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [IndexPageComponent],
-  imports: [CommonModule, IndexRoutingModule, AudioPlayerModule],
+  imports: [
+    CommonModule,
+    IndexRoutingModule,
+    AudioPlayerModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class IndexModule {}
